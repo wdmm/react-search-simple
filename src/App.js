d74cf97f8af50bce307291ab46d6dcb35d6b9855
Vadim Masulis <13vadimm@gmail.com>
2023-01-12 17:43:09 +0200

    react simple search

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Users from './pages/Users';
+import NoPage from './pages/NoPage';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Users />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/NoPage.js
@@ -0,0 +1,13 @@
+import '../App.css';
+
+function NoPage (){
+    return (
+        <div className="App">
+            <header className="App-header">
+            <p>Type your query:</p>
+            </header>
+        </div>
+    );
+};
+
+export default NoPage;
